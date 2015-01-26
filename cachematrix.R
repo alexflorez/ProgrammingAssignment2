a3675911abdafbe04de70fafc3d10b0b32c40bd3
Alex Florez <alex.josueff@gmail.com>
2015-01-25 19:10:08 -0500

    Special Matrix with cache property

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose is to create a spacial "matrix" and cache its inverse.
+## Making use of makeCacheMatrix and cacheSolve
 
-## Write a short comment describing this function
+## makeCacheMatrix creates the "matrix" object and cache its inverse.
+## x is a square matrix and has inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        inv_mat <<- NULL
+    }
+    
+    get <- function() { 
+        x
+    }
+    
+    setInvMat <- function(inverse_matrix) {
+        inv_mat <<- inverse_matrix
+    }
+    
+    getInvMat <- function() {
+        inv_mat
+    }
+    list(set = set, get = get,
+         setInvMat = setInvMat,
+         getInvMat = getInvMat)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the "matrix" returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
